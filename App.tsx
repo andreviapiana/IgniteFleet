40b0fba8583e7009adbeb1b14edee27ac9a1063a
andreviapiana <andreviapiana@gmail.com>
2023-10-07 14:32:45 -0300

    ➕ Instalação do StyledComponents

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import { StatusBar } from 'react-native'
 import { SignIn } from '@screens/SignIn'
 
+import { ThemeProvider } from 'styled-components/native'
+import theme from '@theme'
+
 export default function App() {
   return (
-    <>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+<ThemeProvider theme={theme}>
+  <StatusBar
+    barStyle="light-content"
+    backgroundColor="transparent"
+    translucent
+  />
 
-      <SignIn />
-    </>
+  <SignIn />
+</ThemeProvider>
   )
 }
