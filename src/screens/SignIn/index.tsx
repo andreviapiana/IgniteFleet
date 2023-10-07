ada59c98d4cd16ab0c9976cc0c816e856a5bf5d5
andreviapiana <andreviapiana@gmail.com>
2023-10-07 14:59:52 -0300

    ✨feat: Estrutura - Componente Button

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,12 +2,16 @@ import { Container, Slogan, Title } from './styles'
 
 import backgroundImg from '../../assets/background.png'
 
+import { Button } from '@components/Button'
+
 export function SignIn() {
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
 
       <Slogan>Gestão de uso de veículos</Slogan>
+
+      <Button title="Entrar com Google" />
     </Container>
   )
 }
