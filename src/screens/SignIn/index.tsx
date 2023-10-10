cdf02b6ed28eca7694165ec6cf809bf30dde431f
andreviapiana <andreviapiana@gmail.com>
2023-10-10 15:42:20 -0300

    ✨feat: Login Social - Obtendo Token do Usuário

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,17 +1,54 @@
 import { Container, Slogan, Title } from './styles'
 
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
+
 import backgroundImg from '../../assets/background.png'
 
 import { Button } from '@components/Button'
 
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env'
+import { useEffect, useState } from 'react'
+
+WebBrowser.maybeCompleteAuthSession()
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+  const [_, response, googleSignIng] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ['profile', 'email'],
+  })
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true)
+
+    googleSignIng().then((response) => {
+      if (response?.type !== 'success') {
+        setIsAuthenticating(false)
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      if (response.authentication?.idToken) {
+        console.log('TOKEN DE AUTENTICAÇÃO =>', response.authentication.idToken)
+      }
+    }
+  }, [response])
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
 
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title="Entrar com Google" />
+      <Button
+        title="Entrar com Google"
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
     </Container>
   )
 }
