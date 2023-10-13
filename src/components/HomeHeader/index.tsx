bf47c991248d2e0644c0903750166076a98d6aac
andreviapiana <andreviapiana@gmail.com>
2023-10-13 16:27:46 -0300

    ✨feat: Cloud Mongodb Atlas - Ícones (Phosphor)

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Container, Greeting, Message, Name } from './styles'
 
+import { TouchableOpacity } from 'react-native'
+
+import theme from '@theme'
+import { Power } from 'phosphor-react-native'
+
 export function HomeHeader() {
   return (
     <Container>
@@ -9,6 +14,10 @@ export function HomeHeader() {
 
         <Name>Rodrigo</Name>
       </Greeting>
+
+      <TouchableOpacity>
+        <Power size={32} color={theme.COLORS.GRAY_400} />
+      </TouchableOpacity>
     </Container>
   )
 }
