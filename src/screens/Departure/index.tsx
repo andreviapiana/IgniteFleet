2e99b3a27579f50303f4c32904f5be7a8c527b4c
andreviapiana <andreviapiana@gmail.com>
2023-10-13 17:07:01 -0300

    ✨feat: Interface de Cadastro – Criando Input da Placa

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,12 +1,17 @@
-import { Container } from './styles'
+import { Container, Content } from './styles'
 
 import { Header } from '@components/Header'
+import { LicensePlateInput } from '@components/LicensePlateInput'
 
 export function Departure() {
   return (
     <Container>
       <Container>
         <Header title="Saída" />
+
+        <Content>
+          <LicensePlateInput label="Placa do veículo" placeholder="BRA1234" />
+        </Content>
       </Container>
     </Container>
   )
