2593e0ff2800434b53feac2e9a35db8334937d0d
andreviapiana <andreviapiana@gmail.com>
2023-10-13 19:40:39 -0300

    ✨feat: Banco de Dados Local – Exibindo Dados do Banco

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,12 +5,27 @@ import { CarStatus } from '@components/CarStatus'
 
 import { useNavigation } from '@react-navigation/native'
 
+import { useEffect } from 'react'
+
+import { useQuery } from '../../libs/realm'
+import { Historic } from '../../libs/realm/schemas/Historic'
+
 export function Home() {
+  const historic = useQuery(Historic)
+
   const { navigate } = useNavigation()
 
   function handleRegisterMoviment() {
     navigate('departure')
   }
+
+  function fetchVehicle() {
+    console.log(historic)
+  }
+
+  useEffect(() => {
+    fetchVehicle()
+  }, [])
 
   return (
     <Container>
