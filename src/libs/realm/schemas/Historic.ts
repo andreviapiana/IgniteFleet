ad1b4e465be4432472372cba9f600af36dcc13cf
andreviapiana <andreviapiana@gmail.com>
2023-10-13 16:56:19 -0300

    ✨feat: RealmDB – Método Generate

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,8 +1,32 @@
 /* eslint-disable no-use-before-define */
 import { Realm } from '@realm/react'
 
+type GenerateProps = {
+  user_id: string
+  description: string
+  license_plate: string
+}
+
 export class Historic extends Realm.Object<Historic> {
-  static generate() {}
+  _id!: string
+  user_id!: string
+  license_plate!: string
+  description!: string
+  status!: string
+  created_at!: string
+  updated_at!: string
+
+  static generate({ user_id, description, license_plate }: GenerateProps) {
+    return {
+      _id: new Realm.BSON.UUID(),
+      user_id,
+      description,
+      license_plate,
+      status: 'departure',
+      created_at: new Date(),
+      updated_at: new Date(),
+    }
+  }
 
   static schema = {
     name: 'Historic',
