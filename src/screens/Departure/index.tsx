41fd6cc63667f56539bd2c74e9a600f05347e17b
andreviapiana <andreviapiana@gmail.com>
2023-10-15 18:10:31 -0300

    ✨feat: Geolocalização – Criando o Componente LocationInfo

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -5,6 +5,7 @@ import { LicensePlateInput } from '@components/LicensePlateInput'
 import { TextAreaInput } from '@components/TextAreaInput'
 import { Button } from '@components/Button'
 import { Loading } from '@components/Loading'
+import { LocationInfo } from '@components/LocationInfo'
 
 import { useEffect, useRef, useState } from 'react'
 import {
@@ -38,6 +39,7 @@ export function Departure() {
   const [licensePlate, setLicensePlate] = useState('')
   const [isRegistering, setIsResgistering] = useState(false)
   const [isLoadingLocation, setIsLoadingLocation] = useState(true)
+  const [currentAddress, setCurrentAddress] = useState<string | null>(null)
 
   const [locationForegroundPermission, requestLocationForegroundPermission] =
     useForegroundPermissions()
@@ -109,7 +111,9 @@ export function Departure() {
       (location) => {
         getAddressLocation(location.coords)
           .then((address) => {
-            console.log(address)
+            if (address) {
+              setCurrentAddress(address)
+            }
           })
           .finally(() => setIsLoadingLocation(false))
       },
@@ -146,6 +150,13 @@ export function Departure() {
         >
           <ScrollView>
             <Content>
+              {currentAddress && (
+                <LocationInfo
+                  label="Localização atual"
+                  description={currentAddress}
+                />
+              )}
+
               <LicensePlateInput
                 ref={licensePlateRef}
                 label="Placa do veículo"
