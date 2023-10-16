ca30d7c524b029d242b6c684c158b4a3a482ce4c
andreviapiana <andreviapiana@gmail.com>
2023-10-16 09:38:50 -0300

    ✨feat: Background Tasks – Configurando as Permissões

--- app.config.js
+++ app.config.js
@@ -23,6 +23,9 @@ module.exports = {
       config: {
         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
       },
+      infoPlist: {
+        UIBackgroundModes: ['location'],
+      },
     },
     android: {
       adaptiveIcon: {
@@ -30,6 +33,11 @@ module.exports = {
         backgroundColor: '#202024',
       },
       package: 'com.andreviapiana.ignitefleet',
+      permissions: [
+        'ACCESS_FINE_LOCATION',
+        'ACCESS_COARSE_LOCATION',
+        'ACCESS_BACKGROUND_LOCATION',
+      ],
       config: {
         googleMaps: {
           apiKey: process.env.GOOGLE_MAPS_API_KEY,
