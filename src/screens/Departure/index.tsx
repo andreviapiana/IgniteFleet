f1801d240851369356877ee6f808cb521373c86b
andreviapiana <andreviapiana@gmail.com>
2023-10-16 09:42:49 -0300

    ✨feat: Background Tasks – Obtendo Permissão p/ Obter a Localização em Background

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -28,6 +28,7 @@ import {
   LocationAccuracy,
   LocationObjectCoords,
   LocationSubscription,
+  requestBackgroundPermissionsAsync,
   useForegroundPermissions,
   watchPositionAsync,
 } from 'expo-location'
@@ -57,7 +58,7 @@ export function Departure() {
   const descriptionRef = useRef<TextInput>(null)
   const licensePlateRef = useRef<TextInput>(null)
 
-  function handleDepartureRegister() {
+  async function handleDepartureRegister() {
     try {
       if (!licensePlateValidate(licensePlate)) {
         licensePlateRef.current?.focus()
@@ -82,7 +83,18 @@ export function Departure() {
         )
       }
 
-      setIsResgistering(false)
+      setIsResgistering(true)
+
+      const backgroundPermissions = await requestBackgroundPermissionsAsync()
+
+      if (!backgroundPermissions.granted) {
+        setIsResgistering(false)
+
+        return Alert.alert(
+          'Localização',
+          'É necessário permitir que o App tenha acesso a localização em segundo plano. Acesse as configurações do dispositivo e habilite "Permitir o tempo todo."',
+        )
+      }
 
       realm.write(() => {
         realm.create(
